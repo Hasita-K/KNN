f44b199893194f61e4c857bfe601a3cb8cb0f419
Hasita-K <kodehasita@gmail.com>
2026-09-20 17:00:06 -0400

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kapitbahay.app',
+  appName: 'kapitbahay',
+  webDir: 'dist'
+};
+
+export default config;

5e3900688483c6ab4d09378e3e73f6ff3adc1b81
Hasita-K <kodehasita@gmail.com>
2026-09-21 15:39:10 -0400

    created new gwc file

--- .agents/skills/supabase-server/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("hello gwc");
-
-    return 0;
-}
---
+++ gwc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    prinf("hello gwc");
+
+    return 0;
+}

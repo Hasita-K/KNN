2d6e60d40fc31dd7a77565d59cff468e5d327d28
Hasita-K <kodehasita@gmail.com>
2026-09-20 21:07:38 -0400

    added connectivity at least some connectivity to supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,6 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl =
     import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey =
-    import.meta.env.VITE_SUPABASE_ANON_KEY
+    import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+
+console.log('URL:', supabaseUrl, 'KEY:', supabaseAnonKey)
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)

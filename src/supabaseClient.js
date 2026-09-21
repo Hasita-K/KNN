dc6996dba7c6d74aaf5b3ac1a2b5c0a284022e2c
John Ruiz <jaetr2523@gmail.com>
2026-09-20 22:30:02 -0400

    stuff

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,7 +4,4 @@ const supabaseUrl =
     import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey =
     import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
-
-console.log('URL:', supabaseUrl, 'KEY:', supabaseAnonKey)
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
